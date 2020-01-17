f54df4359726abc75af54721223ce018181b13a1
Jake <jwebb@1904labs.com>
2020-01-17 16:59:20 -0600

    working with linked olKit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { Controls, Map } from '@monsantoit/olKit' // TODO @monsantoco/ol-kit
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Map>
+      <Controls />
+    </Map>
+  )
 }
 
-export default App;
+export default App
