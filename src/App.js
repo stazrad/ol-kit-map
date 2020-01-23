a9b978d7023bc876022db6c882e4cb17d4dce4b3
Jake <jwebb@1904labs.com>
2020-01-23 12:11:12 -0600

    minor update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Controls, Map } from '@monsantoit/olKit' // TODO @monsantoco/ol-kit
 function App() {
   const onMapInit = map => {
     console.log('we got a map!', map)
+    window.map = map
   }
 
   return (
