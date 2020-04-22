75b771f940ac95b9fae5ec5547911bcaa15f3623
MOULDEN, PATRICK [AG-Contractor/1000] <patrick.moulden.ext@bayer.com>
2020-04-22 09:41:56 -0400

    work on dataloader

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React, { useState } from 'react'
-import { Controls, Map, Popup, createDataLayer, loadDataLayer } from '@bayer/ol-kit'
+import { Controls, Map, DataSearchBar } from '@bayer/ol-kit'
 
 import DataLoader from './DataLoader'
-import TimeTicker from './TimeTicker'
 
 function App() {
-  const [dates, setDates] = useState([])
-  const onMapInit = async map => {
+  console.log({ Controls, Map, DataSearchBar }) // eslint-disable-line no-console
+  const [map, setMap] = useState()
+  const onMapInit = map => {
     console.log('we got a map!', map)
     window.map = map
-
-    const dataLayer = await loadDataLayer(map, 'https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson')
-
-    console.log('dataLayer', dataLayer)
-
-    // map.addLayer(createDataLayer('us_counties'), { stroke: {width: 2}})
-    // map.addLayer(createDataLayer('us_waterways'))
-    // map.addLayer(createDataLayer('us_covid'))
+    setMap(map)
   }
 
   return (
-    <Map onMapInit={onMapInit} fullScreen>
-      <DataLoader setDates={setDates} />
-      <Controls />
-      <Popup />
-      <TimeTicker dates={dates} />
-    </Map>
+    <div>
+      <Map onMapInit={onMapInit} fullScreen>
+        {map && <DataLoader map={map} />}
+        <Controls />
+      </Map>
+      <div style={{ height: '100%', width: '500px', backgroundColor: 'white', position: 'absolute' }}>
+        {map && <DataSearchBar maps={[map]}/> }
+      </div>
+    </div>
   )
 }
 
--- src/DataLoader.js
+++ src/DataLoader.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
-import { connectToMap } from '@bayer/ol-kit'
+import { connectToMap, VectorLayer } from '@bayer/ol-kit'
 import proj from 'ol/proj'
 import olFeature from 'ol/feature'
-import olVectorLayer from 'ol/layer/vector'
+// import olVectorLayer from 'ol/layer/vector'
 import olPoint from 'ol/geom/point'
 import olVectorSource from 'ol/source/vector'
 import olStyle from 'ol/style/style'
@@ -11,46 +11,42 @@ import olFill from 'ol/style/fill'
 import olCircleStyle from 'ol/style/circle'
 
 function DataLoader (props) {
-  const { map, setDates } = props
+  const { map } = props
   const dataFetcher = async () => {
-    const dataUrl = 'https://covidtracking.com/api/v1/states/daily.json'
+    const dataUrl = 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases_US/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
     const request = await fetch(dataUrl)
     const data = await request.json()
     const features = []
-    const dates = data.filter(d => d.state === 'MO')
 
-    setDates(dates)
+    data.features.forEach(feat => {
+      if (feat.geometry) {
+        const {x, y} = feat.geometry
+        const coords = proj.fromLonLat([x,y])
+        const feature = new olFeature(new olPoint(coords))
+        const hasConfirmed = feat.attributes.Confirmed > 0
+        const color = hasConfirmed > 0 ? 'rgba(255, 0, 0, 0.4)' : 'rgba(0, 0, 255, 0.4)'
+        const radius = hasConfirmed ? feat.attributes.Confirmed / 250 : feat.attributes.Confirmed
 
-    // data.features.forEach(feat => {
-    //   if (feat.geometry) {
-    //     const {x, y} = feat.geometry
-    //     const coords = proj.fromLonLat([x,y])
-    //     const feature = new olFeature(new olPoint(coords))
-    //     const hasConfirmed = feat.attributes.Confirmed > 0
-    //     const color = hasConfirmed ? 'red' : 'blue'
-    //     const radius = hasConfirmed ? feat.attributes.Confirmed / 250 : feat.attributes.Confirmed
-    //
-    //     feature.setProperties({ ...feat.attributes, title: feat.attributes.Combined_Key })
-    //     feature.setStyle(
-    //       new olStyle({
-    //         image: new olCircleStyle({
-    //           radius,
-    //           fill: new olFill({ color }),
-    //           stroke: new olStroke({
-    //             color,
-    //             width: 1
-    //           })
-    //         })
-    //       })
-    //     )
-    //     feature.getStyle().getImage().setOpacity(.4)
-    //
-    //     features.push(feature)
-    //   }
-    // })
+        feature.setProperties({ ...feat.attributes, radius, color, title: `Confirmed: ${feat.attributes.Confirmed}` })
+        features.push(feature)
+      }
+    })
 
     const source = new olVectorSource({ features })
-    const vectorLayer = new olVectorLayer({ source })
+    const layerStyle = feature => {
+      const { radius = 4, color = 'rgba(255, 0, 0, 0.4)' } = feature?.getProperties?.() || {}
+      return new olStyle({
+        image: new olCircleStyle({
+          radius,
+          fill: new olFill({ color }),
+          stroke: new olStroke({
+            color,
+            width: 1
+          })
+        })
+      })
+    }
+    const vectorLayer = new VectorLayer({ source, title: 'COVID-19', style: layerStyle })
 
     map.addLayer(vectorLayer)
   }
