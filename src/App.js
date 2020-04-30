2efa003e7a3db329ecb9e65590de73470cc7fea7
MOULDEN, PATRICK [AG-Contractor/1000] <patrick.moulden.ext@bayer.com>
2020-04-30 10:46:13 -0400

    make LayerStyler a child of the Map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
-import { Controls, Map, DataSearchBar } from '@bayer/ol-kit'
+import { Controls, Map, LayerStyler } from '@bayer/ol-kit'
 
 import DataLoader from './DataLoader'
 
 function App() {
-  console.log({ Controls, Map, DataSearchBar }) // eslint-disable-line no-console
   const [map, setMap] = useState()
   const onMapInit = map => {
     console.log('we got a map!', map)
@@ -17,10 +16,10 @@ function App() {
       <Map onMapInit={onMapInit} fullScreen>
         {map && <DataLoader map={map} />}
         <Controls />
+        <div style={{ height: '100%', width: '500px', backgroundColor: 'white', position: 'absolute' }}>
+          {map && <LayerStyler /> }
+        </div>
       </Map>
-      <div style={{ height: '100%', width: '500px', backgroundColor: 'white', position: 'absolute' }}>
-        {map && <DataSearchBar maps={[map]}/> }
-      </div>
     </div>
   )
 }
